c2be69459a2bc65b425f5986edf2372ddda0709d
AdriBFK <adrianmagarola@gmail.com>
2024-06-02 01:12:11 +0200

    Update

--- examenjunio/src/main/java/com/mycompany/examenjunio/Estudiante.java
+++ examenjunio/src/main/java/com/mycompany/examenjunio/Estudiante.java
@@ -1,5 +1,8 @@
 package com.mycompany.examenjunio;
 
+import java.io.Serializable;
+import java.util.Arrays;
+
 public class Estudiante{
     private String curso;
     float[][] notas = new float[3][3];
@@ -19,7 +22,25 @@ public class Estudiante{
         return notas;
     }
     @Override
-    public String toString(){
-        String contenido
+        public String toString() {
+        
+        String contenido = "Nombre: " + nombre + "\nApellidos: " + apellidos + " "
+                + "\nDNI: " + dni + "\nCurso: " + curso ;
+        return contenido;
+    }
+    public String toString2() {
+        String contenido = "Nombre: " + nombre + "\nApellidos: " + apellidos + " "
+                + "\nDNI: " + dni + "\nCurso: " + curso + "\n";
+        for(int i=0;i<notas.length; i++)
+        {
+            contenido += "Evaluación " + (i+1) + ":\n";
+            for(int j=0; j<notas[i].length; j++)
+            {
+                contenido += String.format("%.2f", notas[i][j]) + "\t";
+            }
+            contenido += "\n";
+        }
+                    
+        return contenido;
     }
 }
